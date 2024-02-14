9d6132199b61b44b5340d4bf87e6f57dde460a88
itsMePujan <69003888+itsMePujan@users.noreply.github.com>
2024-02-14 22:43:07 +0545

    morgan

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const indexRouter = require("./routes");
 //const aboutRouter = require("./routes/about");
 const app = express();
+const PORT = Number(process.env.PORT);
 
+app.use(morgan("dev"));
 app.use(express.json()); //data as json
+app.use("/assets", express.static("public"));
+
 app.use("/", indexRouter);
 
-app.listen(4000, () => {
-  console.log("App is Running ");
+app.listen(PORT, () => {
+  console.log(`App is Running on PORT ${PORT} `);
 });
